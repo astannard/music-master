5858301df87ff297af6769f24a3068296e30f566
Andy Stannard <stannarda@gmail.com>
2017-05-07 15:37:46 +0100

    adding spotify lookup

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,28 @@
 import React, {Component} from 'react';
 import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
-import './app.css';
+import './App.css';
 
 class App extends Component{
+
+    constructor(props) {
+            super(props);
+            this.state = {
+                query: ""
+            };
+    }
+
+    search(){
+        console.log('state', this.state);
+        const BASE_URL =  'https://api.spotify.com/v1/search?';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+
+        fetch(FETCH_URL,{
+            method: 'GET',
+
+        }).then(response => response.json())
+        .then(json => console.log('json',json));
+    }
+
     render(){
         return(
             <div className="app">
@@ -11,8 +31,15 @@ class App extends Component{
                     <InputGroup>
                         <FormControl 
                             type="text"
-                            placeholder="search for an artist" />
-                            <InputGroup.Addon>
+                            value={this.state.query}
+                            onChange={event => {this.setState({query : event.target.value})}}
+                            onKeyPress={event =>{
+                                if(event.key === 'Enter'){
+                                    this.search();
+                                }}}
+                            placeholder="search for an artist" 
+                        />
+                            <InputGroup.Addon onClick={() => this.search()}>
                                 <Glyphicon glyph="search" />
                             </InputGroup.Addon>
                     </InputGroup>
